41bc84e3fe704fa0935c1451a770d1058bf0fbb6
ribeka <ribekananjo@gmail.com>
2022-09-20 09:27:58 +0900

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-const uri =
-	"mongodb+srv://ribekananjo:Poteribeto06!@cluster0.qfou8eh.mongodb.net/?retryWrites=true&w=majority";
 app.use("/", require("./routes"));
-async function connect() {
-	try {
-		await mongoose.connect(uri);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-connect();
-// app.listen(port, () => {
-//     console.log(`Example app listening on ${port}`)
-
-// })
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
