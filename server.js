8eee4788d07101430f8e400a4dea237eb5de3531
ribeka <ribekananjo@gmail.com>
2022-09-20 09:46:32 +0900

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.use("/", require("./routes"));
+app.use("/", require("./backend/routes"));
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
