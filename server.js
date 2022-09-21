48414b51f97fbcbea70d0634e25993a40da598b0
ribeka <ribekananjo@gmail.com>
2022-09-22 00:58:28 +0900

    update

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const mongodb = require("./config/database");
+const professionalRoutes = require("./routes/professional");
+
+const port = process.env.PORT || 8080;
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-const db = require("./config/database");
-
-app.use(
-	cors({
-		origin: "*",
+app.use(bodyParser.json())
+	.use((req, res, next) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		next();
 	})
-);
+	.use("/professional", professionalRoutes);
 
-app.use("/", require("./routes"));
-app.listen(PORT, () => {
-	console.log(`Connected to DB and listening on ${PORT}`);
+mongodb.initDb((err, mongodb) => {
+	if (err) {
+		console.log(err);
+	} else {
+		app.listen(port);
+		console.log(`Connected to DB and listening on ${port}`);
+	}
 });
