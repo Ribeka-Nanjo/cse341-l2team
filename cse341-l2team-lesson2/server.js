1299075d268c5e7f262ded8b32fa87f391f035f4
ribeka <ribekananjo@gmail.com>
2022-09-22 01:58:47 +0900

    update

--- cse341-l2team-lesson2/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const PORT = process.env.PORT || 8080;
-app.use("/", require("./routes"));
-app.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`);
-});
