cf84ee57805b3052a9c91b5f5d9db9ce81006b44
ribeka <ribekananjo@gmail.com>
2022-09-21 23:55:20 +0900

    updated

--- config/database.js
+++ config/database.js
@@ -1,36 +1,44 @@
-//connnect .env
-const dotenv = require("dotenv");
-dotenv.config();
+// //connnect .env
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-//the content of controller contact.js
-//Store mongo connection
-const MongoClient = require("mongodb").MongoClient;
+// //the content of controller contact.js
+// //Store mongo connection
+// const MongoClient = require("mongodb").MongoClient;
 
-let _db;
+// let _db;
 
-const initDb = (callback) => {
-	if (_db) {
-		console.log("Db is already initialized!");
-		return callback(null, _db);
-	}
-	MongoClient.connect(process.env.MONGODB_URI)
-		.then((client) => {
-			_db = client;
-			callback(null, _db);
-		})
-		.catch((err) => {
-			callback(err);
-		});
-};
+// const initDb = (callback) => {
+// 	if (_db) {
+// 		console.log("Db is already initialized!");
+// 		return callback(null, _db);
+// 	}
+// 	MongoClient.connect(process.env.MONGODB_URI)
+// 		.then((client) => {
+// 			_db = client;
+// 			callback(null, _db);
+// 		})
+// 		.catch((err) => {
+// 			callback(err);
+// 		});
+// };
 
-const getDb = () => {
-	if (!_db) {
-		throw Error("Db not initialized");
-	}
-	return _db;
-};
+// const getDb = () => {
+// 	if (!_db) {
+// 		throw Error("Db not initialized");
+// 	}
+// 	return _db;
+// };
 
-module.exports = {
-	initDb,
-	getDb,
-};
+// module.exports = {
+// 	initDb,
+// 	getDb,
+// };
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+module.exports = mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => console.log("Connected to Mongo Atlas DB"))
+	.catch((err) => console.error(err));
--- routes/professionals.js
+++ routes/professionals.js
@@ -1,7 +1,16 @@
-const express = require("express");
-const professionalController = require("../controllers/professional");
-const router = express.Router();
+// const express = require("express");
+// const professionalController = require("../controllers/professional");
+// const router = express.Router();
 
-router.get("/", professionalController.getData);
-// localhost:8080/professional/
+// router.get("/", professionalController.getData);
+// // localhost:8080/professional/
+
+// module.exports = router;
+
+const router = require("express").Router();
+
+const professionalController = require("../controllers/professionals");
+
+router.get("/", professionalController.getProfessional);
+
 module.exports = router;
