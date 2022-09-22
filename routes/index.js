da4b73da928613614a9d6888fa87b2d308ed3794
ribeka <ribekananjo@gmail.com>
2022-09-22 09:25:06 +0900

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 //const routes = require('express').Router();
 const express = require("express");
 const routes = require("express").Router();
-const contacts = require("../routes/contacts/contacts");
+const contacts = require("./contacts/contacts");
 
 routes.use("/contacts", contacts);
 
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+const db = require("./config/database");
+
+app.use(
+	cors({
+		origin: "*",
+	})
+);
+
 app.use("/", require("./routes"));
 app.listen(PORT, () => {
 	console.log(`Connected to DB and listening on ${PORT}`);
