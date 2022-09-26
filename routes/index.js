385eaf79b3a8573d9e3129c50166742c6b61aab3
Rieba-Nanjo <ribekananjo@gmail.com>
2022-09-26 23:46:32 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./contacts/swagger'));
 router.use('/contacts', require('./contacts/contacts'));
-
-// router.get('/', (req, res) => {
-//   res.send('Hello world');
-// });
 
 module.exports = router;
