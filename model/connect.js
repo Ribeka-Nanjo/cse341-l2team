d3c402eaa7e14c6254c87af79644793b708e8e94
Rieba-Nanjo <ribekananjo@gmail.com>
2022-09-27 07:09:50 +0900

    updated

--- model/connect.js
+++ model/connect.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const contactSchema = mongoose.Schema(
-	{
-		firstName: {
-			type: String,
-		},
-		lastName: {
-			type: String,
-		},
-		email: {
-			type: String,
-		},
-		favoriteColor: {
-			type: String,
-		},
-		birthday: {
-			type: Date,
-		},
-	},
-	{
-		collection: "contacts",
-	}
+  {
+    firstName: {
+      type: String
+    },
+    lastName: {
+      type: String
+    },
+    email: {
+      type: String
+    },
+    favoriteColor: {
+      type: String
+    },
+    birthday: {
+      type: Date
+    }
+  },
+  {
+    collection: 'contacts'
+  }
 );
 
-module.exports = mongoose.model("Contacts", contactSchema);
+module.exports = mongoose.model('Contacts', contactSchema);
